9d022b20ceda35aa2e3ffa8f548e50ae1c4d41e1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-12 18:51:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 class CashRegister
   attr_accessor :total, :discount, :shopping_cart
+  
   @@shopping_cart = []
+
   def initialize(discount = 0)
     @total = 0
     @discount = discount
